5f7cddf0744bd7cbeaf9829a45a59b0c5150b59c
marwene <marwene.elhechmi@sprit.tn>
2018-06-28 09:52:31 +0100

    Wizard + PopUp + Erreur id non trouvé dans les Tabs

--- src/app/layout/wizard/wizard.component.ts
+++ src/app/layout/wizard/wizard.component.ts
@@ -1,27 +1,40 @@
-import { Component, OnInit  } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy  } from '@angular/core';
 import { routerTransition } from '../../router.animations';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { PaysService } from '../../../services/pays.service';
 import { Pays } from '../../../model/model.pays';
+import { ExampleHeader } from './example-header';
+import { MatDialog } from '@angular/material';
+import { WizardPopup } from './wizard-popup';
+import { ProduitsService } from '../../../services/produits.service';
+import { Produit } from '../../../model/model.produit';
 
 
 @Component({
     selector: 'app-wizard-page',
     templateUrl: './wizard.component.html',
     styleUrls: ['./wizard.component.scss'],
-    animations: [routerTransition()]
+    animations: [routerTransition()],
+    encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WizardComponent implements OnInit  {
+  /** @title Datepicker with custom calendar header */
+  exampleHeader = ExampleHeader;
 
   isLinear = false;
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
   thirdFormGroup: FormGroup;
   pays:Pays = new Pays();
+   paysPopup:Pays;
   Paays:any;
   mode:number=1;
-  constructor(private _formBuilder: FormBuilder,public http: HttpClientModule, public paysService:PaysService) { }
+  produit:Produit=new Produit();
+
+  constructor(private _formBuilder: FormBuilder,public http: HttpClientModule,
+    public paysService:PaysService,public produitsService:ProduitsService,public dialog: MatDialog) { }
 
         ngOnInit() {
     this.firstFormGroup = this._formBuilder.group({
@@ -29,8 +42,14 @@ export class WizardComponent implements OnInit  {
       secondCtrl: ['', Validators.required]
     });
     this.secondFormGroup = this._formBuilder.group({
-      thirdCtrl: ['', Validators.required]
+      thirdCtrl: ['', Validators.required],
+      fourthCtrl:['', Validators.required],
+      fifthCtrl:['', Validators.required],
+      sixCtrl:['', Validators.required]
+
     });
+
+
   }
 
   ajoutPays(){
@@ -46,6 +65,35 @@ export class WizardComponent implements OnInit  {
     ,err=>{
       console.log(err);
     })
+  }
+
+  ajoutProduit(){
+    this.produitsService.addProduit(this.produit).subscribe(data=>{
+      console.log(data)
+      this.produit=data;
+      this.mode=2;
+    }
+    ,err=>{
+      console.log(err);
+    })
+  }
+
+
+  openDialog() : void {
+
+    let dialogRef = this.dialog.open(WizardPopup, {
+        width: '700px',
+      data: {
+        paysPop:this.paysPopup
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+        console.log('The dialog was closed');
+        this.paysPopup = result;
+        console.log(result);
+      });
+
   }
 
 }
